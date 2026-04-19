6623061aa288899ba6e922c0d3547af1221b8539
Lohannz <lohan10nascimento@gmail.com>
2026-04-19 13:53:23 -0300

    melhoria na movimentação do player e mais organização

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,38 +1,61 @@
 extends CharacterBody3D
 
-
 const SPEED = 10.0
 const JUMP_VELOCITY = 15.0
+var GRAVITY = 20
+var WALL_SLIDE_VELOCITY = 0.3
 
-# Mecanica de escalar na parede ainda tá meio bugada a questão do double jump e funciona em qualquer parede
-# Fazer com que tenha um unico pulo bonus até encostar no chão novamente
-var climbing = false
-func climb_surface():
-	
-	if is_on_wall() and not is_on_floor() and Input.is_action_pressed("ui_accept"):
-		print("climbing!")
-		velocity.y =  0.0
-		climbing = true
-	else:
-		climbing = false	
-	
-var max_jumps = 2
-var can_jump = max_jumps
-var gravity_y = 20 # Só no eixo y por enquanto
+enum State {GROUNDED, AIRBORNE, CLIMBING }
+var state := State.GROUNDED
+var can_jump := 2 
+
 func _physics_process(delta: float) -> void:
+	_update_state()
+	_handle_gravity(delta)
+	_player_input()
+	move_and_slide()
 	
-	# Add the gravity.
-	if not is_on_floor() and not climbing:
-		velocity.y -= gravity_y * delta
+func _update_state():
+	
+	if is_on_floor():
+		if state != State.GROUNDED:
+			state = State.GROUNDED;
+			can_jump = 2
+	elif _wants_to_climb():
+		if state != State.CLIMBING:
+			state = State.CLIMBING
+			can_jump = 1
 	else:
-		can_jump = max_jumps
+		if state != State.AIRBORNE:
+			state = State.AIRBORNE
+			
+
+# Responsável pela verificação de escadalada (Ex. Layer escaláveis)
+func _wants_to_climb():
+	return is_on_wall() and Input.is_action_pressed("ui_accept")
 	
-	# Pulo já com escalada.
-	if Input.is_action_just_pressed("ui_accept") and can_jump:
-		velocity.y = JUMP_VELOCITY
-		can_jump -= 1
-	climb_surface()
+func _handle_gravity(delta):
+	match state:
+		State.GROUNDED:
+			print("Estou no chão")
+		State.CLIMBING:
+			velocity.y -= WALL_SLIDE_VELOCITY * delta
+			print("Estou escalando")
+		State.AIRBORNE:
+			velocity.y -= GRAVITY * delta
+			print("Estou em queda")	
 		
+	
+var _was_climbing = false
+func _player_input():
+	
+	if Input.is_action_just_pressed("ui_accept") and can_jump > 0:
+		if not  (state == State.CLIMBING and not _was_climbing):
+			velocity.y = JUMP_VELOCITY
+			can_jump -= 1
+			if state == State.CLIMBING:
+				state = State.AIRBORNE	
+	
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -41,5 +64,4 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
+		
