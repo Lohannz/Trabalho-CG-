23c1ba1cd7a3f89357601be06b3202dfe1a08e3a
Lohannz <lohan10nascimento@gmail.com>
2026-06-30 16:50:21 -0300

    Contador e textura do item coletavel. Tá com conflito provavelmente pois o Leo é burro

--- scripts/colecionavel.gd
+++ scripts/colecionavel.gd
@@ -2,8 +2,9 @@ extends Area3D
 
 static var total: int = 0
 
-@onready var mesh = $MeshInstance3D
+@onready var mesh = $mesh_livro
 @onready var collision = $CollisionShape3D
+@onready var camera : Camera3D = get_tree().current_scene.get_node("Camera3D")
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
@@ -13,6 +14,9 @@ func _on_body_entered(body: Node3D) -> void:
 		total += 1
 		_coletar()
 
+func _process(delta: float) -> void:
+	rotate(camera._orientation.y, deg_to_rad(1))
+	
 func _coletar() -> void:
 	mesh.visible = false
 	collision.set_deferred("disabled", true)
--- scripts/contador.gd
+++ scripts/contador.gd
@@ -1,8 +1,21 @@
-extends Label
+extends HBoxContainer
 
-func _ready() -> void:
+@onready var label := $Contador
+@onready var icon_modelo := $icon_livro
+
+func _ready():
 	add_to_group("contador_ui")
 	atualizar(0)
+	var icon = TextureRect.new()
+	icon.texture = textura_item
+	icon.custom_minimum_size = Vector2(60, 60)
+	icon.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+	icon.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
 
-func atualizar(total: int) -> void:
-	text = "Coletados: %d" % total
+	add_child(icon)
+
+var textura_item = preload("res://sprites/icon_livro_coletavel.png")
+var quantidade_atual := 0
+
+func atualizar(total: int):
+	label.text = str(total) + "×"
